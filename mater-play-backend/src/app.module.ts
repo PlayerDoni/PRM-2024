6e95afbe566eaf14ca891d2de3bb654fc8c36668
PlayerDoni <donovanteixeiradelima@gmail.com>
2024-09-20 22:03:24 -0300

    Aula 20/09/2024

---
+++ mater-play-backend/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [], //cd mater-play-backend --> pnpm run start:dev (sobe aplicação)
+  providers: [], 
+})
+export class AppModule {}
