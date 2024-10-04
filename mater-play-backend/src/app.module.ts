7f27be9c9cc15fd277bd567effbc6284282abe22
PlayerDoni <donovanteixeiradelima@gmail.com>
2024-10-03 21:58:43 -0300

    Aula 03/10/2024

--- mater-play-backend/src/app.module.ts
+++ mater-play-backend/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+//cd mater-play-backend --> pnpm run start:dev (sobe aplicação)
 @Module({
-  imports: [],
-  controllers: [], //cd mater-play-backend --> pnpm run start:dev (sobe aplicação)
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres', 
+      host: process.env.BD_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [], 
   providers: [], 
 })
 export class AppModule {}
