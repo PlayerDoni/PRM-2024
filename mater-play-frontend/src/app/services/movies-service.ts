04aa85122b492d1988bef1b049ba31a04678b951
PlayerDoni <donovanteixeiradelima@gmail.com>
2024-11-21 22:07:11 -0300

    Aula (21/11/2024)

--- mater-play-frontend/src/app/services/movies-service.ts
+++ mater-play-frontend/src/app/services/movies-service.ts
@@ -1,5 +1,6 @@
 import { API } from "../@libs/axios";
 import { IMovie } from "../@libs/types";
+import Header from "../components/Header";
 
 
 const _ENDPOINT = '/movies';
@@ -17,6 +18,17 @@ const getMoviesById = async (id: string): Promise<IMovie> => {
 const getByCategoryId = async (id: number): Promise<IMovie[]> => {
     const { data } = await API.get(`${_ENDPOINT}?categoryId=${id}`)
     return data;
+}
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return API.post(`${_ENDPOINT}/upload`, formdata,) {
+        headers: {
+            'Content-Type': 'multipart/from-data'
+            
+        }
+    }
 }
 
 export const MovieService = {
